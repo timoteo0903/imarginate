a15137391d20ef7d924ddb9144c7deb13fd456e7
Timoteo Garica <garciatimoteo03@gmail.com>
2025-02-14 09:40:40 -0300

    First

---
+++ -calculadora/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import MarginCalculator from "@/components/MarginCalculator"
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <MarginCalculator />
+    </main>
+  )
+}
+
