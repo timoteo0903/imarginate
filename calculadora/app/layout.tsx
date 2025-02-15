0ca0a2aa2d767d48beef9da681a52dbcbf1323e5
Timoteo Garica <garciatimoteo03@gmail.com>
2025-02-15 16:18:12 -0300

    Analytics

--- calculadora/app/layout.tsx
+++ calculadora/app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/next';
+
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Footer from "@/components/footer"
